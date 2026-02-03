000e85134ed887b74602f11acfe65c0489616812
Alexey Andreev <myandreev@yandex.ru>
2026-02-03 15:57:23 +0300

    hw-2

---
+++ Django/WelcomeProject/mysite/shopapp/views.py
@@ -0,0 +1,18 @@
+from timeit import default_timer
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def shop_index(request:HttpResponse):
+
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999)
+    ]
+
+    context ={
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
